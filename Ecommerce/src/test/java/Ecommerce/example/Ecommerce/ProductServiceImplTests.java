5fc1d263276ddba33e58faa540379ffd0f1770ed
Doufeni Seif Allah <DoufaniSa@gmail.com>
2024-09-30 16:27:34 +0100

    add unit testing

--- Ecommerce/src/test/java/Ecommerce/example/Ecommerce/ProductServiceImplTests.java
+++ Ecommerce/src/test/java/Ecommerce/example/Ecommerce/ProductServiceImplTests.java
@@ -1,4 +1,3 @@
-/*
 package Ecommerce.example.Ecommerce;
 
 import Ecommerce.example.Ecommerce.Entity.products;
@@ -8,11 +7,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.List;
 
@@ -35,8 +31,8 @@ class ProductServiceImplTests {
         productRepository.deleteAll();
 
         // Add sample products to the database
-        products product1 = new products( "Product Name AAAAA");
-        products product2 = new products( "Product Name BBBBB");
+        products product1 = new products();
+        products product2 = new products();
 
         productRepository.save(product1);
         productRepository.save(product2);
@@ -51,4 +47,3 @@ class ProductServiceImplTests {
         assertThat(productList).hasSize(2); // Check that two products are returned
     }
 }
-*/
